c814a298780d48ef517fcee919b031e448e14531
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-14 19:24:27 +0530

    Third lecture react flow

---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube(){
+    return (
+        <>
+            <h1>YouTube App</h1>
+            <h1>CRA APP</h1>
+        </>
+    )
+}
+
+export default Youtube
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube(){
+    return (
+        <>
+            <h1>YouTube App</h1>
+            <h1>Vite react app</h1>
+        </>
+    )
+}
+
+export default Youtube
