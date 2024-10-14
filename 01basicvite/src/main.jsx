efa8926bd37618c0932267b08646849025f49bc4
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-14 20:02:28 +0530

    4th lecture custom react to learn react

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,38 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+        
+    },
+    childen: 'Click me to visit google'
+}
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom React app</h1>
+        </div>
+    )
+}
+const AnotherElement = (
+    <a href="http://google.com" target='_blank'>Visit Google</a>
+)
 
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: "_blank"},
+    'click to visit google',
+    
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
     <App />
-
+  
 )
