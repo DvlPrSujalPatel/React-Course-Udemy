1f05ce05f2d0300eee1f98c0853d0f061fc231c8
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-15 14:01:14 +0530

    7th lecture new project of password generator learned about useRef useCallback useEffect hooks

---
+++ 05bgchanger/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+function App() {
+  const [color, setcolor] = useState('olive')
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className="outline-none px-4 py-1 rounded-fullshadow-lg text-red-600" onClick={() => setcolor('red')}>Red</button>
+            <button className="outline-none px-4 py-1 rounded-fullshadow-lg text-blue-600" onClick={() => setcolor('blue')}>Green</button>
+            <button className="outline-none px-4 py-1 rounded-fullshadow-lg text-green-600" onClick={() => setcolor('green')}>Blue</button>
+          </div>
+        </div>      
+    </div>
+  )
+}
+
+export default App
